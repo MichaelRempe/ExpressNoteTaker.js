898b98884a4894c57ec380c596de5f2d4000fe2f
MichaelRempe <michaeljrempe@gmail.com>
2020-02-05 09:56:34 -0500

    Note object constructor built  + required

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@
 //====================================================================
 const express = require("express");
 const path = require("path");
+const Note = require("./Note"); //Note Object Constructor
 const app = express(); //create app's server
 
 let PORT = process.env.PORT || 8080; //PORT = Server Default or local machine 8080
@@ -9,6 +10,7 @@ app.listen(PORT, ()=>{
     console.log(`ExpressNoteTaker is listening on @: http://localhost:${PORT}`);
 })
 
+// app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); //Allows for POST data processing
 app.use(express.json());
 
@@ -20,7 +22,8 @@ app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "/public/index.html"));   //ROOT => Landing Page: index.html
 });
 app.get("/notes", (req, res)=>{
-    res.sendFile(path.join(__dirname, "/public/index.html"));   //Notes => File Write Page: notes.html
+    res.sendFile(path.join(__dirname, "/public/notes.html"));   //Notes => File Write Page: notes.html
 });
+
 //====================================================================
 
