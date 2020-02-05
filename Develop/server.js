2addac01f318371d14988133be2c9631cef67107
MichaelRempe <michaeljrempe@gmail.com>
2020-02-05 09:47:09 -0500

    Initial Routing

---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+// Dependencies //
+//====================================================================
+const express = require("express");
+const path = require("path");
+const app = express(); //create app's server
+
+let PORT = process.env.PORT || 8080; //PORT = Server Default or local machine 8080
+app.listen(PORT, ()=>{
+    console.log(`ExpressNoteTaker is listening on @: http://localhost:${PORT}`);
+})
+
+app.use(express.urlencoded({ extended: true })); //Allows for POST data processing
+app.use(express.json());
+
+//====================================================================
+
+// Routes //
+//====================================================================
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/public/index.html"));   //ROOT => Landing Page: index.html
+});
+app.get("/notes", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/public/index.html"));   //Notes => File Write Page: notes.html
+});
+//====================================================================
+
